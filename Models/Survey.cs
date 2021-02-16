5861e380327db5cdf8337352e7a4f3d740f7b5b1
Sujung Choi <schoi0210@gmail.com>
2021-02-15 20:26:45 -0800

    first commit

---
+++ Models/Survey.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurveyModel.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
